53df08c560710a01e629ef5c4ae9c99f2a4f500f
Florian Beck <florian.f.beck@web.de>
2023-06-01 18:39:06 +0200

    Added doctypes

---
+++ hazardous_substance_management/hazardous_item_management/doctype/precautionary_statement/precautionary_statement.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Florian Beck and contributors
+# For license information, please see license.txt
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class PrecautionaryStatement(Document):
+	pass

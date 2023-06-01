1c77751f4db8b35862bc45fa94b70669509d37e4
Florian Beck <florian.f.beck@web.de>
2023-06-01 17:49:11 +0200

    Added icon to desktop settings

--- hazardous_substance_management/config/desktop.py
+++ hazardous_substance_management/config/desktop.py
@@ -4,6 +4,7 @@ def get_data():
 	return [
 		{
 			"module_name": "Hazardous Item Management",
+			"icon": "octicon shield-24",
 			"type": "module",
 			"label": _("Hazardous Item Management")
 		}
